4fbf1d87988005119142789dbdc861c502d360bf
Akety <forson.allann@gmail.com>
2021-02-08 16:11:50 +0100

    ajout de la méthode allan dans Pg

--- src/main/Pg.java
+++ src/main/Pg.java
@@ -3,8 +3,10 @@ package main;
 public class Pg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
+	public void allan() {
+		System.out.println("moi");
+	}
 }
