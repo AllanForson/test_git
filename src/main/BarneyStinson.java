d28c9704a7f9a98360115ea48d3a61247fc8474d
f-baker <fbaker@protonmail.com>
2021-02-08 17:22:47 +0100

    added readme, modified BarneyStinson class because he is the greatest

--- src/main/BarneyStinson.java
+++ src/main/BarneyStinson.java
@@ -3,4 +3,9 @@ public class BarneyStinson {
     public void afficheRiche(){
         System.out.println("All the riches for my fishes");
     }
+    
+    public void wuTangPhraseGenerator(){
+        System.out.println("Back in the game now, got me some weight");
+
+    }
 }
