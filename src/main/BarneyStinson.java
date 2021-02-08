6abe801b74f9afee65ade03b645f3e1f0d644e1f
Akety <forson.allann@gmail.com>
2021-02-08 17:21:34 +0100

    ajout de la mention "package main" et d'une méthode dans la classe Barney

--- src/main/BarneyStinson.java
+++ src/main/BarneyStinson.java
@@ -1,6 +1,12 @@
+package main;
+
 public class BarneyStinson {
 
     public void afficheRiche(){
         System.out.println("All the riches for my fishes");
+    }
+    
+    public void affiche() {
+    	System.out.println("i'm something");
     }
 }
